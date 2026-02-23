056c2b1cf18adf2626619b26d7ca52415f8c9e53
Elivelton Santos <samsepi0l.dev@gmail.com>
2026-02-23 18:15:50 -0300

    refactor change message error 500

--- apps/server/src/presentation/errors/server-error.ts
+++ apps/server/src/presentation/errors/server-error.ts
@@ -1,7 +1,7 @@
 export class ServerError extends Error {
   constructor() {
     super(
-      'O núcleo mágico do servidor (CPU) entrou em colapso. Não foi você, foi o cansaço do dragão que o alimenta.',
+      'O núcleo mágico do servidor entrou em colapso. Não foi você, foi o cansaço do dragão que o alimenta.',
     )
     this.name = 'ServerError'
 
